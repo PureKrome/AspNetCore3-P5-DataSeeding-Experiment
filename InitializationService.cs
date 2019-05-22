b3ee5adeeac619ab6e5caf2abf426edfd4fb1454
Pure Krome <pure.krome@world-domination.com.au>
2019-05-22 22:41:49 +1000

    :space_invader:

---
+++ InitializationService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication7
+{
+    public class InitializationService : IHostedService
+    {
+        private readonly ILogger<InitializationService> _logger;
+
+        public InitializationService(ILogger<InitializationService> logger)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting to do some data initialization.");
+
+            // Read in the names from a file to simulate doing some 'data stuff'
+            var names = await File.ReadAllLinesAsync("Names.txt", cancellationToken);
+
+            _logger.LogInformation($"Rad in {names.Length} names from file.");
+
+
+            // do something like save it to a db.
+            _logger.LogInformation("Storing names into database....");
+
+            await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken); // Stall for 3 seconds.
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            _logger.LogInformation(" ... done!");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IHostedService, InitializationService>();   
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                              .ConfigureLogging(logging =>
+                              {
+                                  logging.ClearProviders();
+                                  logging.AddConsole();
+                              });
+                });
+    }
+}
